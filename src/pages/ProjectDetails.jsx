aa6f1832fd1baff57e892856c83c1a52d9e4be37
Chris Campbell <christopherecampbell@gmail.com>
2023-01-31 21:33:26 -0700

    edit projectDetails

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -4,6 +4,12 @@ const ProjectDetails = () => {
   return (  
   <div className="pageContainer">
     <h1>{projectsArray[0].title}</h1>
+    <p>
+    {projectsArray[0].description}
+    </p>
+    <img src={projectsArray[0].image} alt="app screenshot" width="75%"/>
+    <a href={projectsArray[0].respositoryLink} target="_blank" rel="noreferrer">GitHub</a>
+    <a href={projectsArray[0].deploymentLink} target="_blank" rel="noreferrer">Deployed App</a>
   </div>
   )
 }
