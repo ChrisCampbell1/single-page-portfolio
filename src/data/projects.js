24f456389fb41da9e739bd505823a4e8007b7b9c
Chris Campbell <christopherecampbell@gmail.com>
2023-02-15 12:32:24 -0700

    add FreshStart to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,12 @@
 const projects = [
   {
+    title: "FreshStart",
+    description: "FreshStart is a mobile-first group project. In one week, my team coordinated across 3 time zones to create a social media platform connecting wellness creators with people starting a wellness journey and an API with full CRUD for multiple data resources.",
+    image: "/assets/freshstart.png",
+    respositoryLink: "https://github.com/ChrisCampbell1/fresh-start-front-end",
+    deploymentLink: "https://startfresh.netlify.app/"
+  },
+  {
     title: "Butter",
     description: "Butter is a node.js cookbook application that lets you search, save, import, and copy recipes that you can edit and leave notes on over time. No more guessing on if you used 2 or 3 tablespoons of turmeric in that curry you love. Keeping track of your favorite recipes is as easy as Butter.",
     image: "/assets/butter.png",
