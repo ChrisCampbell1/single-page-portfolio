b7ed3490e8e671f3fb74eff0fa894d824dd3f5b3
Chris Campbell <christopherecampbell@gmail.com>
2023-02-21 12:35:51 -0700

    update project list to carousel and adjust mobile div sizing

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,12 +1,40 @@
 import ProjectCard from "../ProjectCard/ProjectCard";
 import styles from "./ProjectsList.module.css"
+import { useState } from "react";
+
 
 const ProjectsList = (props) => {
+  const [index, setIndex] = useState(0)
+  const length =props.projectsArray.length - 1
+
+  const handlePrevious = () => {
+    const newIndex = index - 1
+    if (newIndex < 0) {
+      return
+    } else {
+      setIndex(newIndex)
+    }
+  }
+
+  const handleNext = () => {
+    const newIndex = index + 1
+    if (newIndex > length) {
+      return
+    } else {
+      setIndex(newIndex)
+    }
+  }
+
   return (
+    // <div className={styles.container}>
+    //   {props.projectsArray.map((project, idx) =>
+    //   <ProjectCard key={idx} project={project} setShowDetails={props.setShowDetails} setProjectState={props.setProjectState}/>
+    //   )}
+    // </div>
     <div className={styles.container}>
-      {props.projectsArray.map((project, idx) =>
-      <ProjectCard key={idx} project={project} setShowDetails={props.setShowDetails} setProjectState={props.setProjectState}/>
-      )}
+      <button onClick={() => handlePrevious()}>Prev</button>
+      <ProjectCard project={props.projectsArray[index]} setShowDetails={props.setShowDetails} setProjectState={props.setProjectState}/>
+      <button onClick={() => handleNext()}>Next</button>
     </div>
   )
 }
